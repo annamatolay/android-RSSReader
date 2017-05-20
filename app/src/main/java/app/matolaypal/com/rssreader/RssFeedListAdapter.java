345825f7565d4d3ef4e262c41f33715d757a773c
Matolay Pal <matolaypal@gmail.com>
2017-05-20 06:50:04 +0200

    done

---
+++ app/src/main/java/app/matolaypal/com/rssreader/RssFeedListAdapter.java
@@ -0,0 +1,49 @@
+package app.matolaypal.com.rssreader;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+class RssFeedListAdapter
+        extends RecyclerView.Adapter<RssFeedListAdapter.FeedModelViewHolder> {
+
+    private List<RssFeedModel> mRssFeedModels;
+
+    static class FeedModelViewHolder extends RecyclerView.ViewHolder {
+        private View rssFeedView;
+
+        FeedModelViewHolder(View v) {
+            super(v);
+            rssFeedView = v;
+        }
+    }
+
+    RssFeedListAdapter(List<RssFeedModel> rssFeedModels) {
+        mRssFeedModels = rssFeedModels;
+    }
+
+    @Override
+    public FeedModelViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_rss_feed, parent, false);
+        return new FeedModelViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FeedModelViewHolder holder, int position) {
+        final RssFeedModel rssFeedModel = mRssFeedModels.get(position);
+        ((TextView)holder.rssFeedView.findViewById(R.id.titleText)).setText(rssFeedModel.title);
+        ((TextView)holder.rssFeedView.findViewById(R.id.descriptionText))
+                .setText(rssFeedModel.description);
+        ((TextView)holder.rssFeedView.findViewById(R.id.linkText)).setText(rssFeedModel.link);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRssFeedModels.size();
+    }
+}
---
+++ app/src/main/java/app/matolaypal/com/rssreader/RssFeedModel.java
@@ -0,0 +1,14 @@
+package app.matolaypal.com.rssreader;
+
+class RssFeedModel {
+
+    String title;
+    String link;
+    String description;
+
+    RssFeedModel(String title, String link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+}
